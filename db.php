86fc339580b89e278c1b9c98d8cd90bd072610f9
HsuShweSinOo <59500650+hsuhsu000@users.noreply.github.com>
2023-08-27 23:55:32 +0630

    Update db.php

--- db.php
+++ db.php
@@ -1,4 +1,4 @@
 <?php
-    $db = pg_connect("host=dpg-cjlo1kocfp5c73aatrvg-a port=5432 dbname=employeedb_zni0
+    $db = pg_connect("host=dpg-cjlo1kocfp5c73aatrvg-a port=5432 dbname=mydb
     user=employeedb_zni0_user password=1NQbTvrejgAU2nilGxDxizkaq5XLVD4Y");
 ?>
